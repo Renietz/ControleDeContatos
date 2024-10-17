3b171c3fe1174fd6c4b72eb5cceead657add3f5b
Renietz <matheusnuernberg@outlook.com>
2024-10-17 20:10:19 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeContatos/Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using ControleDeContatos.Models;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
